aa4e992ac70b7c0275b6baed89928d0c28b0ab1c
Sean Tolbert <sean@seans-mbp.mynetworksettings.com>
2023-10-23 18:36:19 -0400

    adds fake concversation data and preps the index.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,31 +1,81 @@
-import { StyleSheet } from 'react-native';
+import { FlatList, SafeAreaView, StyleSheet } from "react-native";
 
-import EditScreenInfo from '../../components/EditScreenInfo';
-import { Text, View } from '../../components/Themed';
+import EditScreenInfo from "../../components/EditScreenInfo";
+import { Text, View } from "../../components/Themed";
+
+function BubbleMessage({
+  author,
+  message,
+}: {
+  author: string;
+  message: string;
+}) {
+  return (
+    <View
+      style={{
+        maxWidth: "80%",
+        padding: 10,
+        borderRadius: 15,
+        backgroundColor: author === "user" ? "#121212" : "#007aff",
+        alignSelf: author === "user" ? "flex-start" : "flex-end",
+      }}
+    >
+      <Text>{message}</Text>
+    </View>
+  );
+}
 
 export default function TabOneScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <Text style={styles.title}>Tab One</Text>
+        <FlatList
+          data={conversation}
+          renderItem={({ item }) => <BubbleMessage {...item} />}
+          keyExtractor={(_, index) => index.toString()}
+          contentContainerStyle={{
+            // paddingHorizontal: ,
+            gap: 10,
+          }}
+        />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
 });
+
+const conversation = [
+  { author: "bot", message: "Sure what is your location?" },
+  { author: "user", message: "I am in ndowntown san francisco" },
+  {
+    author: "bot",
+    message:
+      "Well Valleys make mountains so htatts about all I have to say about that",
+  },
+  { author: "user", message: "I am in ndowntown san francisco" },
+  {
+    author: "bot",
+    message: "I heard you the first time geez  Broken record over here",
+  },
+  {
+    author: "user",
+    message: "Who are you talking to? youre being disrespectful",
+  },
+];
